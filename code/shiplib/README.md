cd14736ca8ad70080d8f92795ad36e50effbaed0
Frede <Frederiklundbeck@live.dk>
2019-04-02 12:50:26 +0200

    added shiplib with folders hopefully

--- code/shiplib/README.md
+++
@@ -1,1 +0,0 @@
-# The Ships & Sails Code Library :books:
---
+++ code/shiplib/src/main/java/org/dat18c/shiplib/App.java
@@ -0,0 +1,13 @@
+package org.dat18c.shiplib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
