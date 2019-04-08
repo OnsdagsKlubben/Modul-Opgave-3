deb866f44937ca6eff2d1f0c925fedbf03f6a207
Frede <Frederiklundbeck@live.dk>
2019-04-08 11:59:41 +0200

    added spring project

---
+++ code/ships-and-sails/src/main/java/org/dat18c/ships_and_sails/shipsandsails/ShipsAndSailsApplication.java
@@ -0,0 +1,13 @@
+package org.dat18c.ships_and_sails.shipsandsails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipsAndSailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShipsAndSailsApplication.class, args);
+    }
+
+}
---
+++ code/ships-and-sails/src/test/java/org/dat18c/ships_and_sails/shipsandsails/ShipsAndSailsApplicationTests.java
@@ -0,0 +1,16 @@
+package org.dat18c.ships_and_sails.shipsandsails;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShipsAndSailsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
