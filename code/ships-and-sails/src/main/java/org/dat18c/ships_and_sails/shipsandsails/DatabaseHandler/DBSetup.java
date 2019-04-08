0dc8b30159c7879a3a42d8e9e0640310054d2136
marbenknu <marcusbk@hotmail.dk>
2019-04-08 12:34:57 +0200

    Uploaded database connection classes

---
+++ code/ships-and-sails/src/main/java/org/dat18c/ships_and_sails/shipsandsails/DatabaseHandler/DBSetup.java
@@ -0,0 +1,20 @@
+package org.dat18c.ships_and_sails.shipsandsails.DatabaseHandler;
+
+public class DBSetup {
+    private String dbUrl = "";
+    private String username = "";
+    private String password = "";
+
+    public DBSetup(){}
+
+    public String getDbUrl(){
+        return dbUrl;
+    }
+    public String getUsername(){
+        return username;
+    }
+    public String getPassword(){
+        return password;
+    }
+
+}
