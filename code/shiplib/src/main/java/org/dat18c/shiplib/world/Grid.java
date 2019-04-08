f2cf4d5ab4493279cbeb09a14d35ffee1772ae36
Frede <Frederiklundbeck@live.dk>
2019-04-08 13:21:40 +0200

    cleaned code and added more packages

--- code/shiplib/src/main/java/org/dat18c/shiplib/world/Grid.java
+++ code/shiplib/src/main/java/org/dat18c/shiplib/world/Grid.java
@@ -2,6 +2,6 @@ package org.dat18c.shiplib.world;
 
 public class Grid 
 {
-    
+    MapSize size = MapSize.SMALL;
     
 }
---
+++ code/shiplib/src/main/java/org/dat18c/shiplib/world/MapSize.java
@@ -0,0 +1,7 @@
+package org.dat18c.shiplib.world;
+
+public enum MapSize
+{
+    SMALL,
+    BIG
+}
